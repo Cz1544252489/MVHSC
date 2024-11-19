4800472e5f0ce26e650f96f27cad87ba27044f01
Cz1544252489 <git@github.com:Cz1544252489/MVHSC.git>
2024-11-19 19:48:15 +0800

    v1.2.12

--- MVHSC.py
+++ MVHSC.py
@@ -1,32 +1,8 @@
-from typing import Literal
-
-from MVHSC_Aux import create_instances
-import argparse
+from MVHSC_Aux import create_instances, parser
 
 def main():
-    parser = argparse.ArgumentParser(description="None")
-
-    parser.add_argument('--file_name', type=str, default="result.json")
-    parser.add_argument('--view2', type=Literal[0,2,4], default=2)
-    parser.add_argument('--seed_num', type=int, default=42)
-    parser.add_argument('-L','--max_ll_epochs', type= int, default=10, help="下层优化函数内部迭代次数")
-    parser.add_argument('-U','--max_ul_epochs', type=int, default=1, help="上层优化函数内部迭代次数")
-    parser.add_argument('-E','--Epochs', type=int, default=10, help="总迭代次数")
-    parser.add_argument('--s_u', type=float, default=0.5)
-    parser.add_argument('--s_l', type=float, default=0.5)
-    parser.add_argument('--learning_rate', type=float, default=0.01)
-    parser.add_argument('--lambda_r', type=float, default=1.0)
-    parser.add_argument('--mu', type=float, default=0.5)
-    parser.add_argument('--epsilon', type=float, default=0.01)
-    parser.add_argument('--update_lambda_r', type=bool, default=False)
-    parser.add_argument('--result_output', type=str, default="show")
-
-
-    S0 = parser.parse_args()
-    S = vars(S0)
-
-    DI, IN, CL, EV, IT = create_instances(S)
-
+    S = parser()
+    IT = create_instances(S)
     IT.run()
 
 if __name__ == "__main__":
--- MVHSC_Aux.py
+++ MVHSC_Aux.py
@@ -2,6 +2,7 @@
 import math
 import os
 
+import argparse
 os.environ["OMP_NUM_THREADS"] = "1"
 
 from scipy.optimize import linear_sum_assignment
@@ -639,6 +640,28 @@ class evaluation:
 
         return newL2
 
+def parser():
+    parser = argparse.ArgumentParser(description="None")
+
+    parser.add_argument('--file_name', type=str, default="result.json")
+    parser.add_argument('--view2', type=Literal[0,2,4], default=2)
+    parser.add_argument('--seed_num', type=int, default=42)
+    parser.add_argument('-L','--max_ll_epochs', type= int, default=10, help="下层优化函数内部迭代次数")
+    parser.add_argument('-U','--max_ul_epochs', type=int, default=1, help="上层优化函数内部迭代次数")
+    parser.add_argument('-E','--Epochs', type=int, default=10, help="总迭代次数")
+    parser.add_argument('--s_u', type=float, default=0.5)
+    parser.add_argument('--s_l', type=float, default=0.5)
+    parser.add_argument('--learning_rate', type=float, default=0.01)
+    parser.add_argument('--lambda_r', type=float, default=1.0)
+    parser.add_argument('--mu', type=float, default=0.5)
+    parser.add_argument('--epsilon', type=float, default=0.01)
+    parser.add_argument('--update_lambda_r', type=bool, default=False)
+    parser.add_argument('--result_output', type=str, default="show")
+
+    S0 = parser.parse_args()
+    S = vars(S0)
+    return S
+
 def create_instances(S:dict):
     DI = data_importation(view2=S["view2"], seed=S["seed_num"])
     IN = initialization(DI)
@@ -646,6 +669,6 @@ def create_instances(S:dict):
     EV = evaluation(DI, CL)
 
     IT = iteration(EV, IN, S)
-    return DI, IN, CL, EV, IT
+    return IT
 
 
