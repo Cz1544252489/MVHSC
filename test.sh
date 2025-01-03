6a32bb930c01f86e7b72ee1ae2eb2ca33a8e69e4
cz-mini2 <chenzhuo641182084@gmail.com>
2025-01-03 17:21:37 +0800

    promote test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,12 @@
 test1(){
-log_prefix="ADM_orth2"
-python main.py --opt_method "ADM" --log_prefix "${log_prefix}"
-python main.py --opt_method "ADM" --orth_x False --log_prefix "${log_prefix}"
-python main.py --opt_method "ADM" --orth_y False --log_prefix "${log_prefix}"
-python main.py --opt_method "ADM" --orth_x False --orth_y False --log_prefix "${log_prefix}"
-#fields = ["opt_method", "hypergrad_method", "rloop0", "time_cost", "last_UL_dval", "last_LL_dval", "orth_x", "orth_y"]
-
+log_prefix="ADM_orth"
+for i in $(seq 1 100); do
+  echo "$i"
+  python main.py --opt_method "ADM" --log_prefix "${log_prefix}"
+  python main.py --opt_method "ADM" --orth_x False --log_prefix "${log_prefix}"
+  python main.py --opt_method "ADM" --orth_y False --log_prefix "${log_prefix}"
+  python main.py --opt_method "ADM" --orth_x False --orth_y False --log_prefix "${log_prefix}"
+done
+  #fields = ["opt_method", "hypergrad_method", "rloop0", "time_cost", "last_UL_dval", "last_LL_dval", "orth_x", "orth_y"]
 python main_auxx.py "$log_prefix" opt_method rloop0 time_cost last_UL_dval last_LL_dval orth_x orth_y
 }
